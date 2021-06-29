15858742f1f0663fbecfab311d1d846b06255f16
Turmix80 <75222939+Turmix80@users.noreply.github.com>
2021-06-29 21:03:31 +0200

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,50 @@
+'''
+Le script fonctionnne avec les versions 3.8+ de Python et a été testé sur Windows uniquement.
+
+les modules suivants doivent être installés sur la machine source avec pip install:
+-paramiko
+-pysftp
+
+Ce fichier contient les éléments paramétrables selon l'utilisation de chacun.
+
+La copie des éléments est réalisée comme suit:
+
+->les fichiers manquants sont copiés(sauf exclusion(s))
+
+->/!\ en cas de nouvelle version d'un fichier sur la source
+les fichiers plus anciens sur le répertoire distant
+sont écrasés directement sans message d'erreur /!\
+
+une connexion SSH est établie avec un serveur distant
+veuillez renseigner les information de connexion et
+vérifier que le serveur distant est accessible et accepte un login SSH.
+Une connexion root n'est pas nécessaire au bon fonctionnement du script.
+
+Ce fichier de configuration doit être présent sur la machine source, dans le même répertoire que le script principal.
+'''
+
+#renseigner ici la terminaison des fichiers à exclure, ou un élément contenu dans son nom (date, élément..)
+# séparez les éléments par une virgule si il y en à plusieurs
+_EXCLUSION = '.ini' #('.ini', '.txt')
+
+#chemin du répertoire source sur la machine locale /!\ sur Windows, attention aux séparateurs
+# mettre \\ à la place de /
+_SOURCE = "C:\\Users\\Jerem\\testp6\\"
+
+#chemin du répertoire de destination sur la machine distante /!\ sur Windows, attention aux séparateurs
+# mettre \\ à la place de /
+_DEST = "C:\\Users\\turmix\\test_distant_p6\\"
+
+#nom d'utilisateur SSH sur la machine distante
+_USR = 'turmix'
+
+#adresse de la machine distante
+_RMTHOST ='192.168.56.103'
+
+#mot de passe de connexion SSH à la machine distante
+_DESTPASS='chamallow22'
+
+#port de connexion SSH, modifier en cas de port personnalisé
+_PORT=22
+
+
